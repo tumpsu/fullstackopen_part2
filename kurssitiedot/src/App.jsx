b9ab3d396d20200776f6db9f4dda455ed76eca4e
Tuomas Keskitalo <keskitalotuomas@outlook.com>
2026-02-03 19:18:55 +0200

    Copied exercise  1.1-1.5 content to new application.

--- kurssitiedot/src/App.jsx
+++ kurssitiedot/src/App.jsx
@@ -1,34 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Header = ({ course }) => {
+  console.log(course);
+  return <h1>{course.name}</h1>
+}
 
-function App() {
-  const [count, setCount] = useState(0)
+const Part = ({ name, exercises }) => {
+  console.log(name);
+  console.log(exercises);
+  return <p>{name} {exercises}</p>
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      <Part name={course.parts[0].name} exercises={course.parts[0].exercises} />
+      <Part name={course.parts[1].name} exercises={course.parts[1].exercises} />
+      <Part name={course.parts[2].name} exercises={course.parts[2].exercises} />
+    </div>
+  )
+}
+
+const Total = ({ course }) => {
+  console.log(course);
+  const total =
+    course.parts[0].exercises +
+    course.parts[1].exercises +
+    course.parts[2].exercises
+
+  return <p>Number of exercises {total}</p>
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
   )
 }
 
