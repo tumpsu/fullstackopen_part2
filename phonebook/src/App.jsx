515728cc38412ed96e53dea8648803db4f46ed3c
Tuomas Keskitalo <keskitalotuomas@outlook.com>
2026-02-04 21:33:18 +0200

    Excercise: 2.10: puhelinluettelo step5. Refactored compoments to own compoments files.

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,4 +1,8 @@
 import { useState } from 'react';
+import Filter from './compoments/Filter';
+import PersonForm from './compoments/PersonForm';
+import Person from './compoments/Person';
+import Persons from './compoments/Persons';
 
 const App = () => {
   //const [persons, setPersons] = useState([{ name: 'Arto Hellas' } ]); 
@@ -53,28 +57,17 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-         <div>
-        filter shown with: 
-        <input value={filter} onChange={handleFilterChange} />
-      </div>
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
       <h3>Add a new</h3>
-      <form onSubmit={handleSubmit}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-         <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+       <PersonForm 
+          newName={newName} 
+          newNumber={newNumber} 
+          handleNameChange={handleNameChange} 
+          handleNumberChange={handleNumberChange} 
+          handleSubmit={handleSubmit} />
 
       <h2>Numbers</h2>
-      {
-      personsToShow.map((person, i) => (
-        <div key={i}>{person.name} {person.number}</div>
-      ))}
+      <Persons persons={personsToShow} />
     </div>
   )
 }
---
+++ phonebook/src/compoments/Person.jsx
@@ -0,0 +1,8 @@
+const Person = ({ person }) => {
+    return (
+  <div>
+    {person.name} {person.number}
+  </div>
+)
+}
+export default Person;
