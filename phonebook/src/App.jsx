63fe4b51ab51b79c07d5343de38643a72e139877
Tuomas Keskitalo <keskitalotuomas@outlook.com>
2026-02-05 20:46:20 +0200

    Excercise: 2.11: puhelinluettelo step6

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Filter from './compoments/Filter';
 import PersonForm from './compoments/PersonForm';
 import Persons from './compoments/Persons';
@@ -6,15 +7,20 @@ import Persons from './compoments/Persons';
 const App = () => {
   //const [persons, setPersons] = useState([{ name: 'Arto Hellas' } ]); 
   // Test data
-  const [persons, setPersons] = useState([ 
-    { name: 'Arto Hellas', number: '040-123456' }, 
-    { name: 'Ada Lovelace', number: '39-44-5323523' }, 
-    { name: 'Dan Abramov', number: '12-43-234345' }, 
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
   
+  useEffect(() => {
+  console.log('effect')
+  axios
+    .get('http://localhost:3001/persons').then(response => {
+      console.log('promise fulfilled');
+      setPersons(response.data);
+    });
+}, []);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log('Handle submit: ', newName);
